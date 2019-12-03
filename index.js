39c4ebd4e7d986059f66694f02ea9cf21d495ff5
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-03 10:46:35 -0500

    get req on / works

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // implement your API here
-
 // get express module
 const express = require("express");
-
 // create server using express
 const server = express();
-
+// set server to use json parser
+server.use(express.json());
 // get db module from data folder
 const db = require("./data/db.js");
-
 // set port number
 const port = 4000;
+
+server.get("/", (req, res) => {
+  res.send({ api: "up and running :D" });
+});
 
 // set server to listen to port declared above with a message letting me know it's working
 server.listen(port, () => {
