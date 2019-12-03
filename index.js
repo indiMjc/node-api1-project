670a966d21bc612d1f62e182728adada7ecf63f4
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-03 10:42:31 -0500

    set up server to run on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+
+// get express module
+const express = require("express");
+
+// create server using express
+const server = express();
+
+// get db module from data folder
+const db = require("./data/db.js");
+
+// set port number
+const port = 4000;
+
+// set server to listen to port declared above with a message letting me know it's working
+server.listen(port, () => {
+  console.log(`\n***API up and running on port ${port}***\n`);
+});
