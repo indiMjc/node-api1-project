2b697a50ab17b37bcd0cebc3eb6b5f915926650a
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-03 10:53:04 -0500

    get all users req works

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const server = express();
 // set server to use json parser
 server.use(express.json());
 // get db module from data folder
-const db = require("./data/db.js");
+const usersDb = require("./data/db.js");
 // set port number
 const port = 4000;
 
+// initial endpoint to test if working
 server.get("/", (req, res) => {
-  res.send({ api: "up and running :D" });
+  res.send({ message: "ayyy boooi :D" });
+});
+
+// GET /api/users to return array of users from db
+server.get("/api/users", (req, res) => {
+  usersDb
+    .find()
+    .then(users => {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(404).json({ message: "There are no users in the database" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The users could not be retrieved" });
+    });
 });
 
 // set server to listen to port declared above with a message letting me know it's working
